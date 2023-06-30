d2be21d261cc8062f8d27ee5e458569c51df9812
Lawrence <lbuckingham@ozemail.com.au>
2023-06-30 14:05:08 +1000

    Fixed error in shapes - Column and row were transposed tin drawing functions.

--- Sample01/Friday_InheritancePolymorphismInterfaces/Program.cs
+++ Sample01/Friday_InheritancePolymorphismInterfaces/Program.cs
@@ -5,8 +5,148 @@
             Util.ReadInt("Please enter the display height:", out int rows);
             Graphics gfx = new(rows, columns);
 
+            var line = new UpdateableBox(5, 5, 15, 15, '@', columns, rows);
+
+            Scene scene = new(
+                new Line(12, 12, 0, -12),
+                new Line(12, 12, 0, +12),
+                new Line(12, 12, -12, 0),
+                new Line(12, 12, +12, 0),
+                new Box(3, 3, 5, 3),
+                new Box(7, 11, 4, 4),
+                new Diamond(13, 13, 16, 14),
+                new Diamond(17, 11, 10, 10),
+                line
+            );
+
             gfx.DrawText(gfx.Height / 2, gfx.Width / 2 - 3, "Hello!");
-            gfx.Display();
+            while (true) {
+                gfx.Clear();
+                gfx.Draw(scene);
+                gfx.Display();
+                line.Update();
+                Thread.Sleep(100);
+            }
+        }
+    }
+
+    interface IDrawable {
+        void Draw(Graphics gfx);
+    }
+
+    interface IUpdateable {
+        void Update();
+    }
+
+    class Shape : IDrawable {
+        public int Col { get; set; }
+        public int Row { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public char Symbol { get; }
+
+        public Shape(int col, int row, int width, int height, char symbol = '*') {
+            Col = col; Row = row; Width = width; Height = height;
+            Symbol = symbol;
+        }
+
+        public virtual void Draw(Graphics gfx) {
+            // Does nothing
+        }
+
+        public override string ToString() {
+            return $"{GetType().Name}({Col},{Row},{Width},{Height},{Symbol})";
+        }
+
+    }
+
+    class Line : Shape {
+        public Line(int x, int y, int dx, int dy, char symbol = '*')
+            : base(x, y, dx, dy, symbol) {
+        }
+
+        public override void Draw(Graphics gfx) {
+            gfx.DrawLine(Col, Row, Col + Width, Row + Height, Symbol);
+        }
+    }
+
+    class Box : Shape {
+        public Box(int x, int y, int dx, int dy, char symbol = '*')
+            : base(x, y, dx, dy, symbol) { }
+
+        public override void Draw(Graphics gfx) {
+            gfx.DrawLine(Row, Col, Row         , Col + Width, Symbol);
+            gfx.DrawLine(Row, Col, Row + Height, Col        , Symbol);
+
+            gfx.DrawLine(Row + Height, Col        , Row + Height, Col + Width, Symbol);
+            gfx.DrawLine(Row         , Col + Width, Row + Height, Col + Width, Symbol);
+        }
+    }
+
+    class Diamond : Shape {
+        public Diamond(int x, int y, int dx, int dy, char symbol = '*')
+            : base(x, y, dx, dy, symbol) { }
+
+        public override void Draw(Graphics gfx) {
+            gfx.DrawLine(Row, Col + Width / 2, Row + Height / 2, Col + Width, Symbol);
+            gfx.DrawLine(Row, Col + Width / 2, Row + Height / 2, Col        , Symbol);
+
+            gfx.DrawLine(Row + Height / 2, Col        , Row + Height, Col + Width / 2, Symbol);
+            gfx.DrawLine(Row + Height / 2, Col + Width, Row + Height, Col + Width / 2, Symbol);
+        }
+    }
+
+    class UpdateableBox : Box, IUpdateable {
+        int dx = Util.Random(3) - 1;
+        int dy = Util.Random(3) - 1;
+        int w;
+        int h;
+
+        public UpdateableBox(int x, int y, int dx, int dy, char symbol, int w, int h)
+            : base(x, y, dx, dy, symbol) {
+            this.w = w;
+            this.h = h;
+        }
+
+        public override void Draw(Graphics gfx) {
+            base.Draw(gfx);
+            // System.Diagnostics.Debug.WriteLine($"{this}");
+        }
+
+        public void Update() {
+            if (Col + dx < 0) dx = -dx;
+            else if (Col + Width + dx >= w) dx = -dx;
+            else Col = Col + dx; 
+
+            if (Row + dy < 0) dy = -dy;
+            else if (Row + Height + dy >= h) dy = -dy;
+            else Row = Row + dy; 
+        }
+
+        public override string ToString() {
+            return base.ToString() + $" ({dx},{dy})";
+        }
+    }
+
+    class Scene : IDrawable {
+        private List<IDrawable> Children = new();
+
+        public void Draw(Graphics gfx) {
+            foreach (var child in Children) {
+                child.Draw(gfx);
+            }
+        }
+
+        public Scene(params IDrawable[] children) {
+            Children.AddRange(children);
+        }
+
+        public void Add(IDrawable child) {
+            Children.Add(child);
+        }
+
+        public void Remove(IDrawable child) {
+            Children.Remove(child);
         }
     }
 
@@ -48,6 +188,12 @@
                 }
             }
         }
+
+        static Random rand = new(42);
+
+        internal static int Random(int upperLimit) {
+            return rand.Next(upperLimit);
+        }
     }
 
     class Graphics {
@@ -55,9 +201,8 @@
         public int Width => canvas.GetLength(1);
         public int Height => canvas.GetLength(0);
 
-        public Graphics(int rows, int cols )
-        {
-            canvas = new char [rows,cols];
+        public Graphics(int rows, int cols) {
+            canvas = new char[rows, cols];
             Clear();
         }
 
@@ -109,6 +254,10 @@
                 double col = Math.Round(c0 + step * (double) dc / numSteps);
                 DrawPoint((int) row, (int) col, symbol);
             }
+        }
+
+        public void Draw(IDrawable drawableObject) {
+            drawableObject.Draw(this);
         }
     }
 }
